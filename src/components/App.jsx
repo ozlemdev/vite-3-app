d9f340b15ae1fb399fe623da3e761a554da78961
ozlem <06ozlemozturk@gmail.com>
2023-08-03 23:39:50 +0300

    app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,38 @@
-//import CardComp from "./components/CardComp";
-//import TextFieldComp from "./components/TextFieldComp";
-//import Typo from "./components/Typo";
-
+import AppbarComp from "./AppbarComp";
 import CardComp from "./Card.Comp";
 import TextFieldComp from "./TextFieldComp";
 import Typo from "./Typo";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { deepOrange, deepPurple, lime } from "@mui/material/colors";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#bede",
+      },
+      secondary: {
+        main: deepPurple[500],
+      },
+      error: {
+        main: lime["A400"],
+      },
+      koyuMor: {
+        main: deepPurple[500],
+        light: deepPurple[300],
+        dark: deepPurple[800],
+        contrastText: "white",
+      },
+    },
+  });
   return (
     <>
-      {/* <Typo />
-      <TextFieldComp /> */}
-      <CardComp />
+      <ThemeProvider theme={theme}>
+        <Typo />
+        <TextFieldComp />
+        <AppbarComp />
+        <CardComp />
+      </ThemeProvider>
     </>
   );
 }
